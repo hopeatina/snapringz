def4bd1e32bf6e3113203895c49b7bc42450c2b5
Hope Atina <hopeatina@gmail.com>
2016-11-15 12:57:39 -0600

    feature clickable features
    added contact form
    Need content, and clickables

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,6 +10,24 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', ['$scope', '$window', function($scope, $window) {
-  $scope.sectionstyle ={"height": $window.innerHeight-56 + "px"}
+  $scope.sectionstyle ={"height": $window.innerHeight-56 + "px"};
+  $scope.active = [
+      {selected: false, style: {}},
+      {selected: false, style: {}},
+      {selected: false, style: {}},
+      {selected: false, style: {}},
+      {selected: false, style: {}}
+  ];
+  $scope.switchPic = function(picnumber){
+    if (!$scope.active[picnumber].selected) {
+      $scope.active.forEach(function(item){
+        item.style = false;
+      })
+    }
+    $scope.active[picnumber].style = $scope.active[picnumber].selected ? {} : { background: "#7FFF00"};
+
+    console.log($scope.active[picnumber].style, picnumber)
+
+  };
 
 }]);
