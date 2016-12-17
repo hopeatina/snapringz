3dcf61057ebb4adc8410a753c42b28a3731bd530
Hope Atina <hopeatina@gmail.com>
2016-12-17 03:41:44 -0600

    feature: designer changes TODO:  download vectr pics

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,22 +10,51 @@ angular.module('myApp.view1', ['ngRoute'])
     }])
 
     .controller('View1Ctrl', ['$scope', '$window', function ($scope, $window) {
-        $window.Maitre = { uuid: "MF3af70aaa3f" };
-        $scope.sectionstyle = {"height": $window.innerHeight - 56 + "px"};
-        $scope.testyle = $window.innerWidth < 1283 ? { "margin-left": ($window.innerWidth-500)/2 + "px", "margin-right": ($window.innerWidth-500)/2 + "px" } :
-        {"margin-left": ($window.innerWidth-1000)/5 + "px", "margin-right": ($window.innerWidth-1000)/5 + "px"};
+        $window.Maitre = {uuid: "MF3af70aaa3f"};
+        angular.element($window).bind('resize', function () {
+            console.log($window.innerWidth);
+            $scope.sectionstyletest = $window.innerWidth < 1283 ? {"height": (271.4*4) + .3*$window.innerHeight + "px"} : {"height": (271.4*2) + .3*$window.innerHeight + "px"} ;
+            console.log(.1*$window.innerHeight, $scope.sectionstyle);
+            if ($window.innerWidth - 391.25 > 0) {
+                $scope.emaildiv = {"margin-left": ($window.innerWidth - 391.25) / 2 + "px"};
+            }
+            $scope.sectionstyle = {"height": $window.innerHeight - 56 + "px" } ;
+            $scope.testyle = $window.innerWidth < 1283 ? {
+                "margin-bottom": 0+"px"
+            } :
+            {"margin-left": ($window.innerWidth - 1000) / 4.5 + "px", "margin-right": ($window.innerWidth - 1000) / 4.5 + "px", "margin-bottom": 0+"px"};
+            console.log($scope.testyle);
+            if ($window.innerWidth < 1283 && $window.innerWidth > 563) {
+                $scope.testyle["margin-left"] =($window.innerWidth - 500) / 2 + "px";
+                $scope.testyle["margin-right"] =($window.innerWidth - 500) / 2 + "px";
+            }
+        });
+        $scope.sectionstyletest = $window.innerWidth < 1283 ? {"height": (271.4*4) + .3*$window.innerHeight + "px"} : {"height": (271.4*2) + .3*$window.innerHeight + "px"} ;
+        console.log(.1*$window.innerHeight, $scope.sectionstyle);
+        if ($window.innerWidth - 391.25 > 0) {
+            $scope.emaildiv = {"margin-left": ($window.innerWidth - 391.25) / 2 + "px"};
+        }
+        $scope.sectionstyle = {"height": $window.innerHeight - 56 + "px" } ;
+        $scope.testyle = $window.innerWidth < 1283 ? {
+            "margin-bottom": 0+"px"
+        } :
+        {"margin-left": ($window.innerWidth - 1000) / 4.5 + "px", "margin-right": ($window.innerWidth - 1000) / 4.5 + "px", "margin-bottom": 0+"px"};
         console.log($scope.testyle);
+        if ($window.innerWidth < 1283 && $window.innerWidth > 563) {
+            $scope.testyle["margin-left"] =($window.innerWidth - 500) / 2 + "px";
+            $scope.testyle["margin-right"] =($window.innerWidth - 500) / 2 + "px";
+        }
         $scope.smallscreen = $window.innerWidth > 750;
         console.log($window.innerWidth);
         $scope.three60gif = {
             background: "#ffffff url(./assets/neongreen720.gif) 50% 50% no-repeat",
-        /* width: 100%; */
-        margin: 0,
-        height: "100%"
-    };
-    var texts = ["Snap on Tap", "The ultimate secondary accessory", "Because technology...", "For the super-early adopter"];
-    $scope.introtext =  texts[Math.floor(Math.random() * texts.length)];
-    $scope.three60gif["background-size"] = "cover";
+            /* width: 100%; */
+            margin: 0,
+            height: "100%"
+        };
+        var texts = ["Snap on Tap", "The ultimate secondary accessory", "Because technology...", "For the super-early adopter"];
+        $scope.introtext = texts[Math.floor(Math.random() * texts.length)];
+        $scope.three60gif["background-size"] = "cover";
         $scope.active = [
             {selected: false, style: {}},
             {selected: false, style: {}},
@@ -34,7 +63,7 @@ angular.module('myApp.view1', ['ngRoute'])
             {selected: false, style: {}}
         ];
 
-        $scope.switchGif = function (gifnumber){
+        $scope.switchGif = function (gifnumber) {
             console.log(gifnumber);
             if (gifnumber == 1) {
                 $scope.three60gif["background"] = "#ffffff url(./assets/neongreen720.gif) 50% 50% no-repeat"
@@ -54,15 +83,19 @@ angular.module('myApp.view1', ['ngRoute'])
                 })
             }
             $scope.active[picnumber].style = $scope.active[picnumber].selected ? {} : {background: "#7FFF00"};
-            if (picnumber ==0){
+            if (picnumber == 0) {
                 $scope.featuretext = "Activate spectacles";
-            }if (picnumber ==1){
+            }
+            if (picnumber == 1) {
                 $scope.featuretext = "Quick button select";
-            }if (picnumber ==2){
+            }
+            if (picnumber == 2) {
                 $scope.featuretext = "Vibration feedback";
-            }if (picnumber ==3){
+            }
+            if (picnumber == 3) {
                 $scope.featuretext = "Discrete";
-            }if (picnumber ==4){
+            }
+            if (picnumber == 4) {
                 $scope.featuretext = "Charges in case";
             }
             console.log($window.innerWidth);
